5c2efcfa68218f5d543544185a516fdf4fa6e307
get.dipen@gmail.com <get.dipen@gmail.com>
2023-03-09 18:48:21 -0800

    added create and print linked list functions

--- remove-nth-node-from-end.cpp
+++ remove-nth-node-from-end.cpp
@@ -2,6 +2,45 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+ListNode* createLinkedList(vector<int> values) {
+  ListNode* head;
+  ListNode* node = new ListNode();
+  head = node;
+  for (int value : values) {
+    node->next = new ListNode(value);
+    node = node->next;
+  }
+  head = head->next;
+  return head;
+}
+
+// Prints Linked List
+// e.g. 5->8->3->NULL
+void printLinkedList(ListNode* head) {
+  ListNode* node = head;
+
+  while (node != nullptr) {
+    cout << node->val << "->";
+    node = node->next;
+  }
+  cout << "NULL\n";
+}
+
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+}
+
+int main(int argc, char const* argv[]) {
+  ListNode* list = createLinkedList({1, 2, 3, 4, 5});
+  int n = 2;
+  printLinkedList(list);
+  printLinkedList(removeNthFromEnd(list, n));
   return 0;
 }
